933bae3e28822e508885880390d68524ad1204fa
Justin Oakes <justin@justinoak.es>
2015-05-18 19:07:20 -0600

    HK imported

---
+++ Type1/HealthKitController.h
@@ -0,0 +1,14 @@
+//
+//  HealthKitController.h
+//  Type1
+//
+//  Created by Justin Oakes on 5/18/15.
+//  Copyright (c) 2015 Devmtn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+@import HealthKit;
+
+@interface HealthKitController : NSObject
+
+@end
---
+++ Type1/HealthKitController.m
@@ -0,0 +1,13 @@
+//
+//  HealthKitController.m
+//  Type1
+//
+//  Created by Justin Oakes on 5/18/15.
+//  Copyright (c) 2015 Devmtn. All rights reserved.
+//
+
+#import "HealthKitController.h"
+
+@implementation HealthKitController
+
+@end
