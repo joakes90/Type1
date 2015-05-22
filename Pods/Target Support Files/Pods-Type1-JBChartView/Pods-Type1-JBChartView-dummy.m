9057ea5e50987e3bbf5a51b3547035faf1be723b
jonathan thornburg <reliable2192@gmail.com>
2015-05-22 16:50:38 -0600

    picture

---
+++ Pods/Target Support Files/Pods-Type1-JBChartView/Pods-Type1-JBChartView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Type1_JBChartView : NSObject
+@end
+@implementation PodsDummy_Pods_Type1_JBChartView
+@end
---
+++ Type1/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  Type1
+//
+//  Created by jonathan thornburg on 5/22/15.
+//  Copyright (c) 2015 Devmtn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
